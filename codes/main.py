41640784bd79962a3cbb1ba3fdf46d39f9634a7e
mhwahdab <moustafawahdan55@gmail.com>
2021-12-13 17:44:34 +0200

    added gpu support

--- codes/main.py
+++ codes/main.py
@@ -1,14 +1,10 @@
 import numpy as np
 from PIL import Image
-from ISR.models import RRDN,RDN
-
-
+from ISR.models import RRDN
 img = Image.open('sample_image.jpeg')
 #img.resize(size=(img.size[0]*10, img.size[1]*10), resample=Image.BICUBIC)
 lr_img = np.array(img)
 rrdn = RRDN(weights='gans')
-rdn = RDN(weights='noise-cancel')
-sr_img = rrdn.predict(lr_img, by_patch_of_size=50)
-sr_img = rdn.predict(sr_img, by_patch_of_size=50)
+sr_img = rrdn.predict(lr_img)
 dest = Image.fromarray(sr_img)
 dest.save('result.jpeg')
